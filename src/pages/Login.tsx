7a69a139fca33f2ee66b7b66f70bea3ce5f9dbab
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-26 00:37:58 +0100

    feat: implement login page layout with styled components and responsive design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,30 @@
-const Login = () => <h1>Login Page</h1>
+import './Login.scss'
+import Logo from '../assets/logo.svg';
+import LoginImg from '../assets/pablo-sign-in.svg';
+
+const Login = () => {
+    return (
+        <div className="login-page">
+            <div className="login-image">
+                <img src={Logo} alt="Lendsqr Logo" className="login-logo" />
+                <img src={LoginImg} alt='Login image' className='login-img' />
+            </div>
+            <div className="login-container">
+                <div className="login-form">
+                    <div>
+                        <h4>Welcome!</h4>
+                        <p>Enter details to login.</p>
+                    </div>
+                    <div className="input-container">
+                        <input placeholder='Email' />
+                        <input placeholder='Password' />
+                        <a href="">Forgot password?</a>
+                    </div>
+                    <button className='login-btn'>Log In</button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Login
