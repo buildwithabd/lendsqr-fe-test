e010a829194eb7bdb9819e2316e5558546004f88
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-28 14:26:47 +0100

    feat: enhance Layout component with logo and search input for improved navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+
+import './Layout.scss';
+import Logo from '../assets/logo.svg';
+
 
 const Layout = () => (
     <div className="layout">
-        <header></header>
+        <header>
+            <div>
+                <img src={Logo} alt="Lendsqr Logo" className="login-logo" />
+                <input type="search" id="site-search" name="q" placeholder="Search for anything" />
+            </div>
+        </header>
         <aside></aside>
          <main>
             <Outlet />
