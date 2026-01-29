24d7914154e0318750393c0958561ddad8fec3fc
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-29 19:41:56 +0100

    feat: implement password visibility toggle in Login component and update styles for better UX

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 import './Login.scss';
 import Logo from '../assets/logo.svg';
 import LoginImg from '../assets/pablo-sign-in.svg';
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
+
     return (
         <div className="login-page">
             <div className="login-image">
@@ -17,7 +21,16 @@ const Login = () => {
                     </div>
                     <div className="input-container">
                         <input type="email" id="emailAddress" name="email" placeholder='Email' required />
-                        <input type="password" id="userPassword" name="userPassword" placeholder='Password' />
+                        <div className="password-wrapper">
+                            <input type={showPassword ? "text" : "password"} id="userPassword" name="userPassword" placeholder='Password' />
+                            <button 
+                                type="button"
+                                className="password-toggle"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? "Hide" : "Show"}
+                            </button>
+                        </div>
                         <a href="">Forgot password?</a>
                     </div>
                     <button className='login-btn'>Log In</button>
