8b876b346d136ec86c031675010c0b5a558d67d7
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-30 15:12:28 +0100

    feat: enhance Layout component with responsive header and search functionality

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,12 @@ import Logo from '../assets/logo.svg';
 const Layout = () => (
     <div className="layout">
         <header>
-            <div>
+            <div className="header-left">
                 <img src={Logo} alt="Lendsqr Logo" className="login-logo" />
-                <input type="search" id="site-search" name="q" placeholder="Search for anything" />
+                <div className="search">
+                    <input className="search-input" type="search" id="site-search" name="q" placeholder="Search for anything" />
+                    <div className="search-icon"></div>
+                </div>
             </div>
         </header>
         <aside></aside>
