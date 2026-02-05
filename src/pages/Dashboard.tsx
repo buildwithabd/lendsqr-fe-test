3879d9e859dc458b26bf0263234d1ab89141df5b
AfricanBudu <abdullahagboola202@gmail.com>
2026-02-05 15:27:22 +0100

    feat: implement Dashboard component with statistics cards and corresponding styles for enhanced user insights

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,49 @@
-const Dashboard = () => <h1>Dashboard Page</h1>
+import './Dashboard.scss';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard-page">
+            <h1>Users</h1>
+            <div className='stats'>
+                <div className="stat-card">
+                    <div className="stat-icon">
+                        <img src='/components/d_users.svg' alt='users' />
+                    </div>
+                    <div className="stat-content">
+                        <p>USERS</p>
+                        <h2>2,453</h2>
+                    </div>
+                </div>
+                <div className="stat-card">
+                    <div className="stat-icon active-users">
+                        <img src='/components/active_users.svg' alt='active users' />
+                    </div>
+                    <div className="stat-content">
+                        <p>ACTIVE USERS</p>
+                        <h2>2,453</h2>
+                    </div>
+                </div>
+                <div className="stat-card">
+                    <div className="stat-icon loans">
+                        <img src='/components/loan.svg' alt='loans' />
+                    </div>
+                    <div className="stat-content">
+                        <p>USERS WITH LOANS</p>
+                        <h2>12,453</h2>
+                    </div>
+                </div>
+                <div className="stat-card">
+                    <div className="stat-icon savings">
+                        <img src='/components/money.svg' alt='savings' />
+                    </div>
+                    <div className="stat-content">
+                        <p>USERS WITH SAVINGS</p>
+                        <h2>102,453</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Dashboard
